4af2d3c0c4b6753205742e930beda88de42b839e
Toria <v.kalion@yahoo.com>
2025-10-06 02:21:10 +0300

    Add files via upload

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,1 +1,23 @@
+import os
+import sys
 
+# Добавляем путь к нашим классам
+sys.path.insert(0, os.path.abspath('../../src'))
+
+project = 'FASTA Reader'
+copyright = '05.10.2025, Victoria'
+author = 'Victoria'
+release = '1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinxdoc'
+html_static_path = ['_static']
+
+master_doc = 'index'
---
+++ docs/source/index.rst
@@ -0,0 +1,14 @@
+FASTA Reader Documentation
+==========================
+
+.. automodule:: fasta_reader
+   :members:
+
+Classes
+-------
+
+.. autoclass:: Seq
+   :members:
+   
+.. autoclass:: FastaReader  
+   :members:
